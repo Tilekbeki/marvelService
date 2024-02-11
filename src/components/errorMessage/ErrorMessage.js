7c527fe279e1523df4da941f9439ac3897f588d0
tilekbeki <timur.almamatov@yandex.ru>
2024-02-11 18:59:43 +0700

    deleted

--- src/components/errorMessage/ErrorMessage.js
+++
@@ -1,9 +0,0 @@
-import img from './error.gif';
-
-const ErrorMessage = () => {
-    return (
-        <img alt="error" style = {{display: "block", width:"250px", height: "250px", objectFit: "contain", margin: "0 auto"}} src={img} />
-    )
-}
-
-export default ErrorMessage
